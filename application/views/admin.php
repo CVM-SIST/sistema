ec755e221acba1ad828f1d11ed967e0cdce594c8
agonzalezlacoope <agonzalez.lacoope@gmail.com>
2019-10-01 18:38:14 -0300

    imprimir platea

--- application/views/admin.php
+++ application/views/admin.php
@@ -1063,6 +1063,11 @@ $("#comi-activ-form").submit(function(){
             window.open('<?=base_url()?>imprimir/carnet/'+id,'','menubar=yes,toolbar=yes,width=800,height=600');
         })
 
+        $(document).on("click","#imprimir_platea",function(){
+            var id = $(this).data('id');
+            window.open('<?=base_url()?>imprimir/platea/'+id,'','menubar=yes,toolbar=yes,width=800,height=600');
+        })
+
         $("#grupo-select").change(function(){
             var grupo = $(this).val();
             $("#grupo-categorias").slideUp();
