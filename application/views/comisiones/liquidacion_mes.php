cc9e14994f28aa7f8918d462f377448747c61c4a
agonzalezlacoope <agonzalez.lacoope@gmail.com>
2020-04-06 20:33:13 -0300

    Manejo de comisiones que no estaba commiteado

---
+++ application/views/comisiones/liquidacion_mes.php
@@ -0,0 +1,12 @@
+<section class="page page-profile">
+    <div class="panel panel-default">
+        <div class="panel-heading"><strong><span class="fa fa-question-circle"></span> ACCESO SUB-COMISION <?=$nombre_comision?> </strong></div>
+        <div class="panel-body">
+        	<form class="form-horizontal ng-pristine ng-valid" action="<?=$baseurl?>admin/soporte/enviar" method="post">
+                <div class="form-group col-lg-12">
+                    <label for="" class="col-sm-12">Liquidacion del Mes</label>
+               	</div>
+            </form>
+       	</div>
+    </div>
+</section>
