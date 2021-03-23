d6cd3d4223091f0463bfe29d54e45c25f775fc4c
agonzalezlacoope <agonzalez.lacoope@gmail.com>
2021-03-22 23:33:32 -0300

    Agrego linea en blanco p probar 2do factor al pushear

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -4,6 +4,7 @@ class Admin extends CI_Controller {
 
     public function __construct()
     {
+
         parent::__construct();
         $this->load->library(array('session','form_validation'));
         $this->load->helper(array('url','form','date'));
