d41ddb048f45a09058ab0d8386ed642aea6bf757
agonzalezlacoope <agonzalez.lacoope@gmail.com>
2021-03-26 17:42:23 -0300

    Cambie contraseña en la cuenta avisos@clubvillamitre.com

--- application/config/email.php
+++ application/config/email.php
@@ -2,7 +2,7 @@
 $config['protocol'] = 'smtp';                    
 $config['smtp_host'] = 'mail.clubvillamitre.com';
 $config['smtp_user'] = 'avisos@clubvillamitre.com';
-$config['smtp_pass'] = '?Rg6THvRc#u3';
+$config['smtp_pass'] = 'rgb22X$t';
 
 $config['charset'] = 'utf-8';
 $config['wordwrap'] = TRUE;
